69a162ce7bab09b0a2e873f28f3e8a1e88d1ae0d
Huy Nguyen <huuhuy5208@gmail.com>
2023-03-23 02:29:21 +0700

    Add health and score canvas

--- Assets/Scripts/Canvas/HUDController.cs
+++ Assets/Scripts/Canvas/HUDController.cs
@@ -7,14 +7,21 @@ public class HUDController : MonoBehaviour
 {
     public HealthBarContainer _HealthVilage;
     public TextMeshProUGUI _coin;
+    public TextMeshProUGUI _score;
+    public TextMeshProUGUI _health;
+    
     private void Start()
     {
         _HealthVilage.SetMaxHealth(CommonPropeties.healthOfVillage);
         _coin.text = CommonPropeties.coin.ToString();
+        //_score.text = CommonPropeties.score.ToString();
+        _health.text = CommonPropeties.healthOfVillage.ToString();
     }
     private void Update()
     {
         _coin.text = CommonPropeties.coin.ToString();
         _HealthVilage.SetHealth(CommonPropeties.healthOfVillage);
+        //_score.text = CommonPropeties.score.ToString();
+        _health.text = CommonPropeties.healthOfVillage.ToString();
     }
 }
